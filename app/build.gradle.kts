3ab6b826ab5d96dd8a60ef918378c9b912ea962b
fushaoyun4 <fushaoyun4@gmail.com>
2026-07-27 21:48:52 +0800

    fix: kapt worker 单独配 2GB 堆，解决 InvocationTargetException(OOM kill)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,4 +123,8 @@ dependencies {
 kapt {
     correctErrorTypes = true
     useBuildCache = true
+    javacOptions {
+        // kapt worker 单独堆内存，防止注解处理被 OOM kill
+        option("-J-Xmx2048m")
+    }
 }
